d7f435d65cdff7fad6cf76db5b6a79d5f7aa4a18
Peter Hickman <phickman@college.harvard.edu>
2016-04-29 11:54:46 -0400

    updated spreadsheet to final version

--- code/mainPeter.py
+++ code/mainPeter.py
@@ -1,5 +1,6 @@
 from makeRoomingPrefs import MakeRoomingPrefs
 from irving1985 import Irving1985
+import numpy as np
 
 # just pass in the file
 prefsMaker = MakeRoomingPrefs('../roomingQuestionnaireResponses.csv')
@@ -7,12 +8,5 @@ prefsMaker = MakeRoomingPrefs('../roomingQuestionnaireResponses.csv')
 # choose gender and club if desired, can delete to run on all
 prefsList = prefsMaker.prefs(gender="Male", club="Leverett")
 
-# I thought we might need this to get Irving to work, but Irving doesn't work
-prefsList.insert(0, [0] * (len(prefsList) - 1))
-
 # it prints!
 print prefsList
-
-# these don't work
-#irvingMatcher = Irving1985(range(len(prefsList)), prefsList)
-#irvingMatcher.match()
