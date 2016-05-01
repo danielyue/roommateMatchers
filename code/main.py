76e80c19becd635091130fefce9e1da7a86210e6
Daniel Yue <yuedaniel@gmail.com>
2016-04-30 22:19:33 -0400

    working on randomPrefs.py

--- code/main.py
+++ code/main.py
@@ -6,19 +6,19 @@ import numpy as np
 prefsMaker = MakeRoomingPrefs('../roomingQuestionnaireResponses.csv')
 
 # choose gender and club if desired, can delete to run on all
-prefsList = prefsMaker.prefs(gender="Male", club="Leverett")
+prefsList = prefsMaker.prefs()
 
 # I thought we might need this to get Irving to work, but Irving doesn't work
 # prefsList.insert(0, [0] * (len(prefsList) - 1))
 
 prefsList = np.array(prefsList)
+people = np.array(range(len(prefsList)))
 
 # it prints!
-print prefsList
-
+# print prefsList
+# print people
 # import pdb; pdb.set_trace()
-people = np.array(list(range(1,len(prefsList))))
 
 # these don't work
 irvingMatcher = Irving1985(people, prefsList)
-irvingMatcher.match()
+print(irvingMatcher.match())
